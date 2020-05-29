aab71baa321c2db6bad0fea11cf129fb8975abb5
garrett-wood <47695679+garrett-wood@users.noreply.github.com>
2020-05-29 07:16:42 -0400

    Update New-CustomKeyVaultKey.ps1

--- Azure Scripts/New-CustomKeyVaultKey.ps1
+++ Azure Scripts/New-CustomKeyVaultKey.ps1
@@ -1,6 +1,10 @@
 <#This script is used to generate an an Azure KeyVault Key using custom parameters.
 At present, this is the only known way to generate a key with basic contsraints
 modified to specify the entity as a CA that does not involve the compiled code.
+
+The JSON Policy file should be created according to the Azure Key Vault REST API
+documentation:
+https://docs.microsoft.com/en-us/rest/api/keyvault/createcertificate/createcertificate
 #>
 
 # Variables to obtain token
@@ -8,6 +12,7 @@ $TenantId = Read-Host "Enter Tenant ID as Guid"
 $ClientId = Read-Host "Enter Client ID of Authorized Application"
 $ClientSecret = Read-Host "Enter Client Secret"
 $RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
+$Resource = "https://vault.azure.net"
 $body = "grant_type=client_credentials&client_id=$ClientId&client_secret=$ClientSecret&resource=$Resource"
 
 #Obtain Token
@@ -15,7 +20,6 @@ $Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body
 
 #Variables for Key Vault Access and Certificate Creation
 $KeyVaultUrl = Read-Host "Enter FQDN"
-$Resource = "https://vault.azure.net"
 $CertificateName = Read-Host "Enter Requested Certificate Name"
 $PathToJsonPolicy = Read-Host "Enter the relative or full path to your Policy File"
 $ApiUri = "$KeyVaultUrl/certificates/$CertificateName/create?api-version=7.0"
