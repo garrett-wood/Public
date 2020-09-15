4a738ecaf5e728d4e74cd8693a606c1e6e49f22f
garrett-wood <47695679+garrett-wood@users.noreply.github.com>
2020-09-15 17:41:08 -0400

    Update Create-Users.ps1
    Replaced with a more well-defined variant.

--- Directory Scripts/Create-Users.ps1
+++ Directory Scripts/Create-Users.ps1
@@ -20,55 +20,78 @@ for example:
 $CsvPath = Read-Host "Enter Path to CSV"
 $CsvResolvedPath = Resolve-Path $CsvPath
 
-$users = Get-Content $CsvResolvedPath | ConvertFrom-Csv
+$UsersPsList = Get-Content $CsvResolvedPath | ConvertFrom-Csv
 
-#Set absolute variables here, probably change these.
-$OrgUnit = Read-Host "Enter OU DN Here:"
+#Prompts user for OU.
+$Ou = Read-Host "Enter OU DN Here:"
 
-
+#Attempts to Create User account.
 foreach($user in $users)
     {
     
     try
         {
+        Write-Host "Creating User $($user.DisplayName)"
+    
         New-ADUser `
-        -UserPrincipalName $user.email `
-        -EmailAddress $user.email `
-        -SamAccountName ($user.email).Split('@')[0] `
-        -Path $OrgUnit `
-        `
-        -Name $user.displayname `
-        -DisplayName $user.displayname `
-        -GivenName $user.firstname `
-        -Surname $user.lastname `
-        `
-        -Company $Company `
-        -Department $user.department `
-        -City $user.city `
-        -State $user.state `
-        -Country $Country `
-        -Title $user.title `
-        -Description $user.title `
-        `
-        -MobilePhone $user.mobilephone `
-        -AccountPassword ( $user.password | ConvertTo-SecureString -AsPlainText -Force) `
-        -ChangePasswordAtLogon $false `
-        -CannotChangePassword $false `
+        -Name $user.DisplayName `
+        -DisplayName $user.DisplayName `
+        -GivenName $user.GivenName `
+        -Surname $user.Surname `
+        -SamAccountName $user.SamAccountName `
+        -UserPrincipalName $user.UserPrincipalName `
+        -Path $Ou `
+        -AccountPassword ($user.Password | ConvertTo-SecureString -AsPlainText -Force) `
+        -ChangePasswordAtLogon $True `
         -Enabled $true `
-        -Verbose
+        -StreetAddress $user.Address `
+        -City $user.City `
+        -State $user.State `
+        -PostalCode $user.PostalCode `
+        -Country $user.Country `
+        -Title $user.Title `
+        -Department $user.Department `
+        -Division $user.Division `
+        -Office $user.Office `
+        -Company $user.Company `
+        -Description $user.Title `
+        -EmailAddress $user.UserPrincipalName `
+        -MobilePhone $User.Mobile `
+        -OfficePhone $User.Phone `
+        -Fax $user.Fax `
         }
      
     #If user does not have sufficient rights, terminate script and warn.
     catch [System.UnauthorizedAccessException]
         {
-        Write-Warning "The operation failed due to insufficient access rights. Are you running this as Administrator?"
-        Return
+        Write-Warning "The operation to create user $($user.UserPrincipalName) failed due to insufficient access rights. Terminating script."
+        Exit
         }
     
     #If there is a pre-existing user with the conflicy, warn user, but continue running.
     catch [Microsoft.ActiveDirectory.Management.ADIdentityAlreadyExistsException],[Microsoft.ActiveDirectory.Management.ADException]
         {
-        Write-Warning "User $($user.displayname) already exists. Skipping user."
+        Write-Warning "The operation to create user $($user.UserPrincipalName) because the user already exists."
+        }
+    
+    }
+
+
+#Attempts to Set Managers for User Account. This is a seperate step to ensure the manager account is created before attempting to assign them to a user.
+foreach($user in $users)
+    {
+    
+    try
+        {
+        Write-Host "Setting Manager for user $($user.DisplayName) to $($user.ManagerAltDisplayName)"
+        Set-ADUser -Identity $user.SamAccountName -Manager $user.ManagerSamAccountName
+        }
+     
+    #If user does not have sufficient rights, terminate script and warn.
+    catch [System.UnauthorizedAccessException]
+        {
+        Write-Warning "The operation to assign set manager for user $($user.UserPrincipalName) as $($user.ManagerAltDisplayName) failed due to insufficient access rights. Terminating script."
+        Exit
         }
     
     }
