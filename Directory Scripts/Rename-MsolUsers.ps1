ae8a22d076b104e757ee25e0f8c9aaec08a40c6b
garrett-wood <47695679+garrett-wood@users.noreply.github.com>
2020-09-23 12:43:31 -0400

    Update Rename-MsolUsers.ps1

--- Directory Scripts/Rename-MsolUsers.ps1
+++ Directory Scripts/Rename-MsolUsers.ps1
@@ -1,8 +1,11 @@
+# This script will rename any users with a UPN matching the default onmicrosoft domain with a UPN using your chosen suffix.
+
 $users = Get-MsolUser -All | Where-Object UserPrincipalName -Like "*onmicrosoft.com" | Where-Object Firstname -ne $null | Select *
-$newdomain = Read-Host "Enter new domain name
+$newdomain = Read-Host "Enter new domain name"
+
 ForEach ($user in $users)
-{
-$newupn = $user.UserPrincipalName.Split('@')[0] + $newdomain
-Set-MsolUserPrincipalName -ObjectId $user.ObjectId -NewUserPrincipalName $newupn
-Write-Host "User Named:" $user.UserPrincipalName "name was changed to" $newupn
-}
+    {
+    $newupn = $user.UserPrincipalName.Split('@')[0] + $newdomain
+    Set-MsolUserPrincipalName -ObjectId $user.ObjectId -NewUserPrincipalName $newupn
+    Write-Host "User Named: $($user.UserPrincipalName) name was changed to $($newupn)"
+    }
